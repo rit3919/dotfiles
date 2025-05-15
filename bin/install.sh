0508f834e6465ce638bb4daff5932117a66b0f3c
41986326+rit3919 <41986326+rit3919@users.noreply.github.com>
2025-05-16 00:40:58 +0900

    fix:change shell to zsh

--- bin/install.sh
+++ bin/install.sh
@@ -17,6 +17,7 @@ function main(){
 
 	# bashFlag
 	local bash="false"
+	local noupdate="false"
 
 	while [ "$#" -gt 0 ]; do
 		case "$1" in
@@ -24,6 +25,8 @@ function main(){
 				usage;exit 0;;
 			--bash)
 				bash="true";;
+			-u|--no-update)
+				noupdate="true";;
 			
 			*)
 				;;
@@ -32,13 +35,24 @@ function main(){
 	done
 	
 	# update pacman
-	# pacman -Syu --noconfirm
+	if [ "$noupdate" = false ]; then
+		sudo pacman -Syu --noconfirm
+	fi	
 
 	# check if zsh exists
 	if !(type "zsh" > /dev/null 2>&1); then
-		pacman -S --noconfirm zsh
+		sudo pacman -S --noconfirm zsh
 	else
 		echo "already exists zsh."
+	fi
+
+	if [ "$bash" = true ]; then
+		echo "Default Login Shell: Bash"
+	else
+		chsh -s /bin/zsh $USER
+		if [ $? == "0" ]; then
+			echo "Change Login Shell"
+		fi
 	fi
 	
 }
