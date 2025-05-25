8db23ffc6f74a7d47a27e0650f954b4619be8f69
41986326+rit3919 <41986326+rit3919@users.noreply.github.com>
2025-05-25 18:53:24 +0900

    add:link .zshrc .config

--- bin/install.sh
+++ bin/install.sh
@@ -27,6 +27,14 @@ function main(){
 	local is_update="false"
 	# pacman -S installFlag.
 	local is_pkg_install="false"
+	
+	# bash_backup path
+	local backup_directory="bash_backup"
+
+	# config_backup path
+	local config_directory="config_backup"
+
+	local dotfiles_path="$HOME/dotfiles"
 
 	while [ "$#" -gt 0 ]; do
 		case "$1" in
@@ -51,12 +59,12 @@ function main(){
 		sudo pacman -Syu --noconfirm
 
 		if [ "$is_pkg_install" = true ]; then
-			sudo pacman -S --needed --noconfirm - < ../etc/package.list
+			sudo pacman -S --needed --noconfirm - < $dotfiles_path/config/package.list
 		else
 			read -p "Do you want to install a package from the package.list? (y/N)" ans
 			case "$ans" in
 				[Yy]*)
-					sudo pacman -S --needed --noconfirm - < ../etc/package.list;;
+					sudo pacman -S --needed --noconfirm - < $dotfiles_path/config/package.list;;
 				"" | N*)
 					;;
 			esac
@@ -75,10 +83,44 @@ function main(){
 		echo "Default Login Shell: Bash"
 	else
 		chsh -s /bin/zsh $USER
+
+		#backup bashfiles
+		if [ ! -d $HOME/$backup_directory ]; then
+			mkdir $HOME/${backup_directory}
+			mv $HOME/.bash* $HOME/$backup_directory
+		else
+			read -p  "Warning: backup files already exists. Do you want to overwrite? (y/N)" ans
+			case $ans in
+				[Yy]*)
+					mv $HOME/.bash* $HOME/$backup_directory;;
+				"" | N*)
+					echo "please check your backup files"
+					exit;;
+			esac
+		fi
+		
+		# Link .zsh
+		ln -s $dotfiles_path/zsh/.zshrc $HOME/.zshrc
 	fi
 
+	if [ -d $HOME/.config ]; then
+		if [ ! -d $HOME/$config_directory ]; then
+			mkdir $HOME/$config_directory
+			mv $HOME/.config $HOME/$config_directory
+		else
+			read -p "Warning: .config already exists. Do you wan to overwrite (y/N)" ans
+			case $ans in
+				[Yy]*)
+					mv $HOME/.config $HOME/$config_directory;;
+				"" | N*)
+					echo "please check your .config direcotry"
+					exit;;
+			esac
+		fi
+	fi
 
-	
+	# Link .config			
+	ln -s $dotfiles_path/config $HOME/.config
 }
 
 main "$@"
